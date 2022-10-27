5f8e86861d25ab0ba6b39aa64265b6f76430116a
dandrue <d.ruedaq@gmail.com>
2022-10-27 13:01:42 -0500

    Rule learning looking for convergence

--- RuleLearning.py
+++ RuleLearning.py
@@ -2,15 +2,9 @@
 import time
 from DefaultStructure import *
 from BackProp import *
-from multiprocessing import Pool
+import FuzzyControllerGuide as FCG
+from threading import Thread
 
-start = time.time()
-tVar = FuzzyVariable(name="Temperature", rang=[100, 340], labels=["Fria", "Fresca", "Normal", "Tibia", "Caliente"])
-pVar = FuzzyVariable(name='Pressure', rang=[10, 250], labels=["Escasa", "Baja", "Bien", "Fuerte", "Alta"])
-aVar = FuzzyVariable(name='Action', rang=[-60, 60], labels=["NG", "NM", "NP", "CE", "PP", "PM", "PG"])
-Rules = RuleGenerator([pVar, tVar, aVar])
-rules = Rules.gencomb()
-# print(rules)
 
 def controller(tempvalue, presvalue, objective):
     # rules = {"Escasa": {"Fria": "PG", "Fresca": "PG", "Normal": "PM", "Tibia": "PM", "Caliente": "PP"},
@@ -58,6 +52,7 @@ def controller(tempvalue, presvalue, objective):
             DicValIntersec.pop(i)
 
     # print("New \n", DicValIntersec)
+    # TODO Standarization of the output variables, lists and dictionaries
 
     NG = []
     NM = []
@@ -80,60 +75,114 @@ def controller(tempvalue, presvalue, objective):
         for j in DicValIntersec[i].keys():
             rulesc[i][j] = [DicValIntersec[i][j]] * 7
             ruleslist.append([DicValIntersec[i][j]] * 7)
-    # print("rules c\n \n", rulesc, "\n")
     # print(ruleslist)
     counter = 0
     # ex es una ejemplo de las posibles funciones de pertenencia a la salida
     ex = aVar.labels
-    # print('ex', ex)
     totalComb = []
-    totalValue = []
+    global totalError, cError, pError
     totalError = []
+    cError = []
+    pError = []
+    print(len(ruleslist))
+    # TODO cambiar el código para cuando las pertenencias son diferentes de 4
     for i in range(len(ruleslist[0])):
-        for j in range(len(ruleslist[1])):
-            for k in range(len(ruleslist[2])):
-                for m in range(len(ruleslist[3])):
-                    Acc = deepcopy(AccionDict)
-                    # print(ex[i], ex[j], ex[k], ex[m])
-                    Acc[ex[i]].append(ruleslist[0][i])
-                    Acc[ex[j]].append(ruleslist[1][j])
-                    Acc[ex[k]].append(ruleslist[2][k])
-                    Acc[ex[m]].append(ruleslist[3][m])
-                    counter += 1
-                    # print("Acc ", Acc)
-                    # entry1 = Acc
-                    # print(counter)
-                    totalComb.append(Acc)
-                    # print("before", DictAccSal)
-                    # print(Acc)
-                    tcon = totalc(Acc, DictAccSal, objective)
-                    # print("after", DictAccSal)
-                    # print(Acc)
-                    pcon = partialc(Acc, DictAccSal, objective)
-                    totError = (tcon + pcon)/2
-                    totalError.append(totError)
-                    # print('Entry 1', entry1)
-                    # totalValue.append(tcon)
-                    # print('Ciclo ', counter, " Finalizado")
-                    # print(totError)
-                    # print("Acc 2", Acc)
+        # print(ruleslist[0])
+        try:
+            for j in range(len(ruleslist[1])):
+                # print(ruleslist[1])
+                try:
+                    for k in range(len(ruleslist[2])):
+                        # print(ruleslist[2])
+                        try:
+                            for m in range(len(ruleslist[3])):
+                                # print(ruleslist[3])
+                                Acc = deepcopy(AccionDict)
+                                Acc[ex[i]].append(ruleslist[0][i])
+                                Acc[ex[j]].append(ruleslist[1][j])
+                                Acc[ex[k]].append(ruleslist[2][k])
+                                Acc[ex[m]].append(ruleslist[3][m])
+                                counter += 1
+                                totalComb.append(Acc)
+                                tcon = totalc(Acc, DictAccSal, objective)
+                                pcon = partialc(Acc, DictAccSal, objective)
+                                cError.append(tcon)
+                                pError.append(pcon)
+                                totError = tcon
+                                totalError.append(totError)
+                        except IndexError:
+                            if tot <=175:
+                                Acc = deepcopy(AccionDict)
+                                Acc[ex[i]].append(ruleslist[0][i])
+                                Acc[ex[j]].append(ruleslist[1][j])
+                                Acc[ex[k]].append(ruleslist[2][k])
+                                counter += 1
+                                totalComb.append(Acc)
+                                tcon = totalc(Acc, DictAccSal, objective)
+                                pcon = partialc(Acc, DictAccSal, objective)
+                                cError.append(tcon)
+                                pError.append(pcon)
+                                totError = tcon
+                                totalError.append(totError)
+                            else:
+                                pass
+                except IndexError:
+                    if tot <=175:
+                        Acc = deepcopy(AccionDict)
+                        Acc[ex[i]].append(ruleslist[0][i])
+                        Acc[ex[j]].append(ruleslist[1][j])
+                        counter += 1
+                        totalComb.append(Acc)
+                        tcon = totalc(Acc, DictAccSal, objective)
+                        pcon = partialc(Acc, DictAccSal, objective)
+                        cError.append(tcon)
+                        pError.append(pcon)
+                        totError = tcon
+                        totalError.append(totError)
+                    else:
+                        pass
+        except IndexError:
+            if tot <= 175:
+                Acc = deepcopy(AccionDict)
+                Acc[ex[i]].append(ruleslist[0][i])
+                counter += 1
+                totalComb.append(Acc)
+                tcon = totalc(Acc, DictAccSal, objective)
+                pcon = partialc(Acc, DictAccSal, objective)
+                cError.append(tcon)
+                pError.append(pcon)
+                totError = tcon
+                totalError.append(totError)
+            else:
+                pass
 
-    # print(totalError)
-    # print(len(totalError))
-    # plotting(totalError)
     rulesl = deepcopy(rulesc)
-
+    # try:
+    #     plotting()
+    # except IndexError:
+    #     pass
     for i in rulesl.keys():
         for j in rulesl[i].keys():
             rulesl[i][j] = []
     c = 0
+
+
+
     for i in range(len(totalError)):
         # print(i)
-        if totalError[i] <= np.quantile(totalError, 0.25):
         # if totalError[i] <= np.mean(totalError):
         # if totalError[i] <= 0.1:
-            rulesl = getrule(i, ruleslist, rulesl, ex)
-            c += 1
+        objlist = [objective] * len(cError)
+        rui = [np.abs(e1 - e2) for e1, e2 in zip(objlist, cError)]
+        if np.sign(cError[i]) == np.sign(objective): # and np.abs(cError[i]-objective) <= np.quantile(rui, 0.05):
+            oblist = [objective] * len(pError[i])
+            res = [np.abs(e1 - e2) for e1, e2 in zip(oblist, pError[i])]
+            for j in pError[i]:
+                if np.sign(j) == np.sign(objective): # and np.abs(j-objective) <= np.quantile(rui, 0.025):
+                    print(j, objective)
+                    # print("change")
+                    rulesl = getrule(i, ruleslist, rulesl, ex)
+                    c += 1
     # print(c)
     # print(rulesl)
     # print(rules)
@@ -142,21 +191,28 @@ def controller(tempvalue, presvalue, objective):
         temp = deepcopy(rules[i])
         for j in rulesl[i].keys():
             # print(rules[i][j])
-            temp[j] = rulesl[i][j]
+            if len(rulesl[i][j]) != 0:
+                t = []
+                for k in rulesl[i][j]:
+                    if k in temp[j]:
+                        t.append(k)
+                if len(t) != 0:
+                    temp[j] = t
         rules[i] = temp
 
     # print(rulesl)
     print(rules)
 
 
-def plotting(totalError):
+
+def plotting():
     fig, axs = plt.subplots(2, 2)
     fig.suptitle('Error distribution analysis')
 
     sns.lineplot(ax=axs[0, 0], x=range(len(totalError)), y=totalError)
     axs[0, 0].set_title("Total Error Lineplot")
-    axs[0, 0].set_xlabel("Combination")
-    axs[0, 0].set_ylabel("Total Error")
+    axs[0, 0].set_xlabel("Total Error")
+    axs[0, 0].set_ylabel("Count")
 
     sns.histplot(ax=axs[0, 1], x=totalError, kde=True)
     axs[0, 1].axvline(x=np.mean(totalError), color='b', label='Mean Error')
@@ -206,6 +262,7 @@ def getrule(ind, ruleslist, rulesl, ex):
     # print(rulesl)
     return rulesl
 
+
 def totalc(AccionDict, DictAccSal, objective):
     # print('total contribution')
     AccionConorma = deepcopy(AccionDict)
@@ -225,24 +282,19 @@ def totalc(AccionDict, DictAccSal, objective):
         for i in AccionConorma.keys():
             if DictAccSal[i].type == "TrapAbiertaIzquierda":
                 Base = DictAccSal[i]
-                AccDict[i] = TrapAbiertaIzquierda("TrapIzquierdaTruncada", Base.var,
-                                                     [Base.inicio, Base.medio,
-                                                      Base.fin],
+                AccDict[i] = TrapAbiertaIzquierda("TrapIzquierdaTruncada", Base.var, [Base.inicio, Base.medio, Base.fin],
                                                      AccionConorma[i])
             elif DictAccSal[i].type == "TrapAbiertaDerecha":
                 Base = DictAccSal[i]
-                AccDict[i] = TrapAbiertaDerecha("TrapDerechaTruncada", Base.var,
-                                                   [Base.inicio, Base.medio, Base.fin],
+                AccDict[i] = TrapAbiertaDerecha("TrapDerechaTruncada", Base.var, [Base.inicio, Base.medio, Base.fin],
                                                    AccionConorma[i])
             elif DictAccSal[i].type == "Trapezoidal":
                 Base = DictAccSal[i]
-                AccDict[i] = Trapezoidal('TrapTruncada', Base.var,
-                                            [Base.inicio, Base.medio1, Base.medio2, Base.fin],
+                AccDict[i] = Trapezoidal('TrapTruncada', Base.var, [Base.inicio, Base.medio1, Base.medio2, Base.fin],
                                             AccionConorma[i])
             elif DictAccSal[i].type == "Triangular":
                 Base = DictAccSal[i]
-                AccDict[i] = Triangular('TriangularTruncada', Base.var,
-                                           [Base.inicio, Base.medio, Base.fin],
+                AccDict[i] = Triangular('TriangularTruncada', Base.var, [Base.inicio, Base.medio, Base.fin],
                                            AccionConorma[i])
 
     elif Tipo == "Truncado":
@@ -250,26 +302,22 @@ def totalc(AccionDict, DictAccSal, objective):
             if DictAccSal[i].type == "TrapAbiertaIzquierda":
                 Base = DictAccSal[i]
                 medio = Base.evalx(AccionConorma[i])
-                AccDict[i] = TrapAbiertaIzquierda("TrapIzquierdaTruncada", Base.var,
-                                                     [Base.inicio, medio, Base.fin],
+                AccDict[i] = TrapAbiertaIzquierda("TrapIzquierdaTruncada", Base.var, [Base.inicio, medio, Base.fin],
                                                      altura=AccionConorma[i])
             elif DictAccSal[i].type == "TrapAbiertaDerecha":
                 Base = DictAccSal[i]
                 medio = Base.evalx(AccionConorma[i])
-                AccDict[i] = TrapAbiertaDerecha("TrapDerechaTruncada", Base.var,
-                                                   [Base.inicio, medio, Base.fin],
+                AccDict[i] = TrapAbiertaDerecha("TrapDerechaTruncada", Base.var, [Base.inicio, medio, Base.fin],
                                                    altura=AccionConorma[i])
             elif DictAccSal[i].type == "Trapezoidal":
                 Base = DictAccSal[i]
                 medio1, medio2 = Base.evalx(AccionConorma[i])
-                AccDict[i] = Trapezoidal('TrapTruncada', Base.var,
-                                            [Base.inicio, medio1, medio2, Base.fin],
+                AccDict[i] = Trapezoidal('TrapTruncada', Base.var, [Base.inicio, medio1, medio2, Base.fin],
                                             altura=AccionConorma[i])
             elif DictAccSal[i].type == "Triangular":
                 Base = DictAccSal[i]
                 medio1, medio2 = Base.evalx(AccionConorma[i])
-                AccDict[i] = Trapezoidal('TriangularTruncada', Base.var,
-                                            [Base.inicio, medio1, medio2, Base.fin],
+                AccDict[i] = Trapezoidal('TriangularTruncada', Base.var, [Base.inicio, medio1, medio2, Base.fin],
                                             altura=AccionConorma[i])
     Areas = []
     Centroides = []
@@ -286,7 +334,7 @@ def totalc(AccionDict, DictAccSal, objective):
     # print(SA)
     value = round(XA / SA, 3)
 
-    cerror = np.abs(value-objective)/aVar.rng
+    cerror = value
     # print(cerror)
     return cerror
 
@@ -295,7 +343,6 @@ def partialc(AccionDict, DictAccSal, objective):
     AccionConorma = deepcopy(AccionDict)
     AccDict = deepcopy(AccionDict)
     # print("AccionConorma", AccionConorma)
-
     # El diccionario AccionConorma contiene el valor de corte para cada uno de los conjuntos difusos a la salida
     # print("AccionConorma\n \n", AccionConorma, "\n")
 
@@ -369,22 +416,67 @@ def partialc(AccionDict, DictAccSal, objective):
 
     perror = []
     for i in Centroides:
-        perror.append(np.abs(objective - i)/aVar.rng)
-    # print(Centroides)
-    # print(sum(perror))
+        perror.append(i)
 
-    # XA = [a * b for a, b in zip(Areas, Centroides)]
-    # XA = sum(XA)
-    # # print(XA)
-    # SA = sum(Areas)
-    # # print(SA)
-    # value = round(XA / SA, 3)
-    # print(value)
-    # return value
-    return np.mean(perror)
+    return perror
 
 
-controller(150, 80, 31.443)
-end = time.time()
+def probe():
+    ob = np.ones(25)
+    length = np.zeros_like(ob)
+    counter = 0
+    for i in rules.keys():
+        for j in rules[i].keys():
+            val = len(rules[i][j])
+            length[counter] = val
+            counter += 1
+    tot = np.dot(length, ob)
+    print(np.dot(length, ob))
+    return all(ob ==length)
 
-print("The time of execution of above program is :", (end - start)*10**3, "ms")
+def outerLoop():
+    rul = deepcopy(rules)
+    tRange = tVar.rang
+    pRange = pVar.rang
+    while probe() != True:
+        tempValues = np.random.uniform(tRange[0], tRange[1])
+        presValues = np.random.uniform(pRange[0], pRange[1])
+        actionValues = FCG.controller(tempValues, presValues)
+        controller(tempValues, presValues, actionValues)
+    print(rul)
+    print(rules)
+
+
+def outerloop(samples):
+    rul = deepcopy(rules)
+    tRange = tVar.rang
+    pRange = pVar.rang
+    tempValues = np.random.randint(tRange[0], tRange[1], [samples])
+    presValues = np.random.randint(pRange[0], pRange[1], [samples])
+    actionValues = [FCG.controller(tempValues[i], presValues[i]) for i in range(samples)]
+    #print(actionValues)
+    for i in range(samples):
+        controller(tempValues[i], presValues[i], actionValues[i])
+        print("Loop", i)
+    print(rul)
+    print(rules)
+
+# controller(150, 80, 31.443)
+# outerloop(100)
+# probe()
+
+if __name__ == '__main__':
+    start = time.time()
+    global tot
+    tot = 175
+    tVar = FuzzyVariable(name="Temperature", rang=[100, 340], labels=["Fria", "Fresca", "Normal", "Tibia", "Caliente"])
+    pVar = FuzzyVariable(name='Pressure', rang=[10, 250], labels=["Escasa", "Baja", "Bien", "Fuerte", "Alta"])
+    aVar = FuzzyVariable(name='Action', rang=[-60, 60], labels=["NG", "NM", "NP", "CE", "PP", "PM", "PG"])
+    Rules = RuleGenerator([pVar, tVar, aVar])
+    rules = Rules.gencomb()
+    # print(rules)
+    # rules = {'Escasa': {'Fria': ['PM', 'PG'], 'Fresca': ['PM', 'PG'], 'Normal': ['PP', 'PM', 'PG'], 'Tibia': ['PP', 'PM', 'PG'], 'Caliente': ['CE', 'PP', 'NP']}, 'Baja': {'Fria': ['PP', 'PM', 'PG'], 'Fresca': ['PP', 'PM'], 'Normal': ['CE', 'PP'], 'Tibia': ['CE', 'NP'], 'Caliente': ['CE', 'PP', 'NP']}, 'Bien': {'Fria': ['CE', 'PP', 'NP'], 'Fresca': ['CE', 'NP'], 'Normal': ['CE', 'NP'], 'Tibia': ['NP', 'NM'], 'Caliente': ['NP', 'NM', 'NG']}, 'Fuerte': {'Fria': ['NM', 'NP', 'CE'], 'Fresca': ['NG', 'NM', 'NP'], 'Normal': ['NM', 'NP'], 'Tibia': ['NG', 'NM', 'NP'], 'Caliente': ['NG', 'PG']}, 'Alta': {'Fria': ['NG', 'NM', 'NP'], 'Fresca': ['NG', 'NM'], 'Normal': ['PG', 'NG', 'NM', 'NP'], 'Tibia': ['PG', 'NG'], 'Caliente': ['PG', 'NG', 'NM', 'NP']}}
+    outerLoop()
+    end = time.time()
+
+    print("The execution time is :", (end - start)*10**3, "ms")
